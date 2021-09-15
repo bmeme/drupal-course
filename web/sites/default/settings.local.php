ef01746e810b2a7ee7494af39e5da5af296e3b2e
Daniele Piaggesi <daniele.piaggesi@bonsaimeme.com>
2021-09-15 12:15:48 +0200

    fixed settings

---
+++ web/sites/default/settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$config['system.performance']['css']['preprocess'] = FALSE;
+$config['system.performance']['js']['preprocess'] = FALSE;
